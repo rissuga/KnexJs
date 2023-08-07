2e5574468aaa4db7c14a2fca1959ef272378bc78
risa <risairma21@gmail.com>
2023-08-07 10:57:46 +0700

    refactor: change table user

--- db/migrations/20230803024732_users.js
+++ db/migrations/20230803024732_users.js
@@ -6,6 +6,8 @@ exports.up = function(knex) {
     return knex.schema.createTable('users', function(table) {
         table.increments('id').primary();
         table.string('name').notNullable();
+        table.string('email').notNullable();
+        table.string('password').notNullable();
         table.integer('role_id').unsigned().references('id').inTable('role');
     })
   
